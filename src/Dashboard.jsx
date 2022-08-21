6d389ee7d5137d51e63ea8bc960fafefbd462103
GerardGhub <singian28@gmail.com>
2022-08-22 01:21:02 +0800

    Adding Validation in Register With hooks PO Master

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 function Dashboard(props) {
+
+    //    executes only once - on initial render = componentdidmount
+    useEffect(() => {
+        document.title ="Dashboard - eCommerce";
+    }, []);
+
     return (
         <div>
             <h1>Dashboard</h1>
