85d560ba6a4999c7045ce2ab69b6fb943e9f9937
GerardGhub <singian28@gmail.com>
2022-08-21 21:12:02 +0800

    Adding UseEffects

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,41 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 let Login = () => {
-   var [email, setEmail] = useState("");
-   var [password, setPassword] = useState("");
+    var [email, setEmail] = useState("");
+    var [password, setPassword] = useState("");
+
+    // executes on each render (initial render & state updates)
+    useEffect(() => {
+
+        // console.log(email, password);
+    });
+
+
+    // executes only on state updates of "email" only and also with the initial render
+    useEffect(() => {
+        // validation on email only
+        if (email.indexOf("@") > 0) {
+            // console.log("valid");
+        } else {
+            // console.log("invalid");
+        }
+
+    }, [email])
+
+
+    //    executes only once - on initial render = componentdidmount
+    useEffect(() => {
+        document.title ="Login - eCommerce";
+    }, []);
+
+    // executes only once on component unmounting phase = componentWillUnmount
+    useEffect(() => {
+        //do something
+        return () => {
+            console.log("Component Unmount");
+        };
+    });
+
 
     return (
         <div className="row">
@@ -21,21 +54,21 @@ let Login = () => {
                                 id="email"
                                 name="email"
                                 value={email}
-                                onChange={(event) => {setEmail(event.target.value);}}
+                                onChange={(event) => { setEmail(event.target.value); }}
                                 placeholder="Email">
                             </input>
                         </div>
                         {/* email ends */}
 
-                          {/* password start */}
-                          <div className="form-group">
+                        {/* password start */}
+                        <div className="form-group">
                             <label htmlFor="passwordl">Password</label>
                             <input type="password"
                                 className="form-control"
                                 id="password"
                                 name="password"
                                 value={password}
-                                onChange={(event) => {setPassword(event.target.value);}}
+                                onChange={(event) => { setPassword(event.target.value); }}
                                 placeholder="Password">
                             </input>
                         </div>
