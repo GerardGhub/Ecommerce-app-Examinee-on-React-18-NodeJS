f9893e1c7eb7409116259e67a74a5c5bf6c929a9
GerardGhub <singian28@gmail.com>
2022-08-21 14:36:00 +0800

    Implementing Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import Login from "./Login";
+import Register from "./Register";
+import NoMatchPage from "./NoMatchPage";
+import Dashboard from "./Dashboard";
+import { HashRouter, Routes } from "react-router-dom";
+import { Route } from "react-router";
 
 function App() {
-    return <Login/>;
+    return (
+        <HashRouter>
+            <div className="container-fluid">
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="*" element={<NoMatchPage />} />
+                </Routes>
+            </div>
+        </HashRouter>
+    );
 }
 
 export default App;
---
+++ src/NoMatchPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let NoMatchPage = (props) => {
+    return (
+       <h1 className="className text-danger">Page Not Found</h1>
+    );
+}
+
+export default NoMatchPage;
