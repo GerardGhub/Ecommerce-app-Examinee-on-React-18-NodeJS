8411412ec130e479aa0521a1829ef119f89f39ba
GerardGhub <singian28@gmail.com>
2022-08-21 13:19:50 +0800

    React Exam

---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+let Login = () => {
+    return (
+        <div className="row">
+            <div className="col-lg-5 col-md-7 mx-auto">
+                <div className="card border-success shadow-lg my-2">
+                    <div className="card-header border-bottom border-success">
+                        <h4>Login</h4>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+};
+
+export default Login;
+
